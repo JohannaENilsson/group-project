08d8455816d43a723111861d392cfea3df71fe9b
Anna Eklöv <annaeklov90@gmail.com>
2020-02-25 14:42:34 +0100

    added header

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Header from "./Header.js";
 
 export default function Home() {
     return (
         <div>
             <h2>Home</h2>
+            <Header />
         </div>
     );
 }
