c6c50e0cd19d17f5b9dff58011b127951930d418
Anna Eklöv <annaeklov90@gmail.com>
2020-02-25 15:13:14 +0100

    header added

--- src/components/Folder.js
+++ src/components/Folder.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import Header from "./Header.js";
+
 
 export default function Folder() {
     return (
         <div>
+        <Header />
             <h2>Folder</h2>
         </div>
     );
