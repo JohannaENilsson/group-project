851428c326ee262c5d7b0066ae1807805780a0cb
Johanna Nilsson <nijodotcom@gmail.com>
2020-03-05 12:24:21 +0100

    breadCumbs works as links

--- src/components/InnerContainer.js
+++ src/components/InnerContainer.js
@@ -3,29 +3,13 @@ import { Dropbox } from 'dropbox';
 import { Link, useHistory, useLocation } from 'react-router-dom';
 
 import { token$ } from './Store.js';
-
 import GetAllFiles from '../actions/GetAllFiles';
 import PopupAddNewFolder from '../actions/PopupAddNewFolder';
 
-export default function InnerContainer({ fileList, getFiles, onDelete,}) {
-
-  let history = useHistory(); // använd för breadcrums
-  // console.log('history ', history);
-
-  let location = useLocation();
-  // console.log('locatopn ----->', location)
-
+export default function InnerContainer({ fileList, getFiles, onDelete }) {
+  const history = useHistory(); // använd för breadcrums
   let breadcrums = history.location.pathname.split('/').splice(1);
-  let urlPath = history.location.pathname.split('/').splice(1);
- 
-  console.log('Breadcrums ', breadcrums);
-  console.log('url ', urlPath);
-
-// if path === folder byta till home
-
-// breadcrumbs. joina med /
-
-let linkToUrl = ''
+  let linkToUrl = '';
 
   return (
     <div className='innerContainer'>
@@ -33,13 +17,19 @@ let linkToUrl = ''
         linkToUrl += `/${path}`;
         console.log('Curr url ', linkToUrl);
 
-        return (
-        <div key={path}>
-          <Link to={`${linkToUrl}`}>{path}</Link>
-
-        </div>
+        if (path === 'folder') {
+          return (
+            <div key={'Home'}>
+              <Link to={`/home`}>Home</Link>
+            </div>
           );
-
+        } else {
+          return (
+            <div key={path}>
+              <Link to={`${linkToUrl}`}>{path}</Link>
+            </div>
+          );
+        }
       })}
       <h2 className='innerContainerTitle'></h2>
       <GetAllFiles
